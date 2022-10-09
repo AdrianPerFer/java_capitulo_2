0f865af4ea45dbe6cc141910dbec48b9ea936f33
Adrian <chopon007@gmail.com>
2022-10-09 15:07:08 +0200

    Exercise2 now is Ejercicio2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -2,7 +2,7 @@
 pantalla de tal forma que el resultado del programa sea el mismo que en el 
 ejercicio 1 del capitulo 1. */
 
-public class Exercise2 {
+public class Ejercicio2 {
     public static void main(String[] args) {
         // Variables "nombre" created
         String nombre = "Adrián Perogil Fernández";
