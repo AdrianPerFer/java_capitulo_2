d6274b916c504dbb34fbec7f69aa6ef25899f437
Adrian <chopon007@gmail.com>
2022-10-09 15:03:23 +0200

    Exercise2 added

---
+++ Exercise2.java
@@ -0,0 +1,11 @@
+/* Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+pantalla de tal forma que el resultado del programa sea el mismo que en el 
+ejercicio 1 del capitulo 1. */
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        // Variables "nombre" created
+        String nombre = "Adrián Perogil Fernández";
+        System.out.println(nombre);
+    }
+}
