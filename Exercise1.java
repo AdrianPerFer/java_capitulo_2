efafeb2aa3330c35f60377c9f44fe25e30c68024
Adrian <chopon007@gmail.com>
2022-10-09 14:29:02 +0200

    Exercise1 added

---
+++ Exercise1.java
@@ -0,0 +1,27 @@
+/* Escribe un programa en el que se declaran las variables enteras x, y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación. */
+public class Exercise1 {
+    public static void main(String[] args) {
+        // Variables
+        int x = 144;
+        int y = 999;
+        // Salidas por pantallas
+        System.out.println("Valor de x: " + x);
+        System.out.println("------------------------------");
+        System.out.println("Valor de y: " + y);
+        System.out.println("------------------------------");
+        System.out.println("Suma de x e y: "+(x + y));
+        System.out.println("------------------------------");
+        System.out.println("Resta de x e y: "+(x - y));
+        System.out.println("------------------------------");
+        System.out.println("Resta de y e x: "+(y - x));
+        System.out.println("------------------------------");
+        System.out.println("Multiplicación de x e y: "+(x * y));
+        System.out.println("------------------------------");
+        System.out.println("División de x e y: "+(x / y));
+        System.out.println("------------------------------");
+        System.out.println("División de y e x: "+(y / x));
+        System.out.println("------------------------------");
+    }
+}
