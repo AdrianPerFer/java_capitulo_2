e4bdf1c07eb671c176888a0836cb06d20bb5c93a
Adri <chopon007@gmail.com>
2022-10-10 08:35:13 +0200

    Ejercicio3 added

---
+++ Ejercicio3.java
@@ -0,0 +1,23 @@
+/******************************************************************************************
+ * 3.   Crea las variables nombre, direccion y telefono y asígnales los valores           *
+ *      correspondientes. Muestra los valores de esas variables por pantalla de tal       *
+ *      forma que el resultado del programa sea el mismo que en el ejercicio 2.           *
+ *                                                                                        *
+ ******************************************************************************************
+ *                                                                    |  |                *
+ *                                                                    |  |                *
+ *                          @author Adrián Perogil Fernández        *      *              *
+ *                                                                   ******               *
+ ******************************************************************************************/
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        // Variables
+        String nombre = "Adrián Perogil Fernández";
+        String direccion = "Calle Alozaina 3, Bloque Venecia 2, 2ºE";
+        String telefono = "622274396";
+        // Outputs
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+    }
+}
