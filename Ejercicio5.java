5d9912822569be6ffb525d9320ab5d6ff7d77e05
Adrian <chopon007@gmail.com>
2022-10-11 20:11:22 +0200

    Ejercicio5 changed

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,31 +1,24 @@
-/******************************************************************************************
- * 5.   Realiza un conversor de pesetas a euros. La cantidad en euros que se quiere       *
- *      convertir deberá estar almacenada en una variable.                                *
- *                                                                                        *
- *                                                                                        *
- ******************************************************************************************
- *                                                                    |  |                *
- *                                                                    |  |                *
- *                          @author Adrián Perogil Fernández        *      *              *
- *                                                                   ******               *
- ******************************************************************************************/
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         2.   Variables                                                           *
+    *                                                                                  *
+    *         5.   Realiza un conversor de pesetas a euros. La cantidad en pesetas     *
+    *              que se quiere convertir deberá estar almacenada en una variable.    *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio5 {
     public static void main(String[] args) {
-        // Scanner
-        Scanner sc = new Scanner(System.in);
-        // Try - Catch for errors
-        try {
-            // Variables pesetas and total
-            System.out.println("Introduce la cantidad de pesetas: ");
-            float pesetas = sc.nextFloat();
-            double total = (pesetas / 166.386);
-            // Output
-            System.out.println(pesetas + " son " + (float)total + " euros");
-        } catch (InputMismatchException ex) {
-            System.out.println("Dato introducido erróneo");
-        } sc.close();
+        // Variables "pesetas", "euros"
+        int pesetas = 1333;
+        double euros = (pesetas / 166.386);
+        // Output
+        System.out.printf("%d pesetas son %.2f euros. \n", pesetas, euros);
     }
 }
