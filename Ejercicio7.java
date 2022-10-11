d9de22e06ff232e9a143bdaa276994fd2680e133
Adrian <chopon007@gmail.com>
2022-10-11 08:51:05 +0200

    Ejercicio7 added

---
+++ Ejercicio7.java
@@ -0,0 +1,20 @@
+/******************************************************************************************
+ * 7.   Escribe un programa que declare variables de tipo char y de tipo string. Intenta  *
+ *      mostrarías por pantalla todas juntas en la misma linea y con una sola sentencia   *
+ *      de Java (con un solo println) ¿es posible?.                                       *
+ *                                                                                        *
+ ******************************************************************************************
+ *                                                                    |  |                *
+ *                                                                    |  |                *
+ *                          @author Adrián Perogil Fernández        *      *              *
+ *                                                                   ******               *
+ ******************************************************************************************/
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        // Variable
+        char letra = 'a';
+        String palabra = "ejemplo";
+        // Output
+        System.out.println("String: " + palabra + ", Char: " + letra);
+    }
+}
