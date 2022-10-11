32ca27250df53427bc4896da100e29747e06c45a
Adrian <chopon007@gmail.com>
2022-10-11 19:54:20 +0200

    Ejercicio1 changed

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,17 +1,23 @@
-/******************************************************************************************
- * 1.   Escribe un programa en el que se declaran las variables enteras x, y. Asígnales   *
- *      los valores 144 y 999 respectivamente. A continuación, muestra por pantalla       *
- *      el valor de cada variable, la suma, la resta, la división y la multiplicación.    *
- *                                                                                        *
- ******************************************************************************************
- *                                                                    |  |                *
- *                                                                    |  |                *
- *                          @author Adrián Perogil Fernández        *      *              *
- *                                                                   ******               *
- ******************************************************************************************/
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         2.   Variables                                                           *
+    *                                                                                  *
+    *         1.   Escribe un programa en el que se declaren las variables             *
+    *              enteras x e y. Asígnales los valores 144 y 999 respectivamente.     *
+    *              A continuación, muestra por pantalla el valor de cada variable,     *
+    *              la suma, la resta, la división y la multiplicación.                 *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio1 {
     public static void main(String[] args) {
-        // Variables
+        // Variables "x", "y"
         int x = 144;
         int y = 999;
         // Outputs
