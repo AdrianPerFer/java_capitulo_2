8ad46de2d701be23e0ce1a2c04093a48e565c198
Adrian <chopon007@gmail.com>
2022-10-11 20:23:41 +0200

    Ejercicio7 changed

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,20 +1,27 @@
-/******************************************************************************************
- * 7.   Escribe un programa que declare variables de tipo char y de tipo string. Intenta  *
- *      mostrarías por pantalla todas juntas en la misma linea y con una sola sentencia   *
- *      de Java (con un solo println) ¿es posible?.                                       *
- *                                                                                        *
- ******************************************************************************************
- *                                                                    |  |                *
- *                                                                    |  |                *
- *                          @author Adrián Perogil Fernández        *      *              *
- *                                                                   ******               *
- ******************************************************************************************/
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         2.   Variables                                                           *
+    *                                                                                  *
+    *         7.   Escribe un programa que declare variables de tipo char y            *
+    *              de tipo string. Intenta mostrarías por pantalla todas juntas        *
+    *              en la misma linea y con una sola sentencia de Java                  *
+    *              (con un solo println) ¿es posible?.                                 *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio7 {
     public static void main(String[] args) {
-        // Variable
-        char letra = 'a';
+        // Variable 
+        char letra1 = 'a';
+        char letra2 = 'd';
         String palabra = "ejemplo";
-        // Output
-        System.out.println("String: " + palabra + ", Char: " + letra);
+        // Outputs
+        System.out.println("String: " + palabra + ", Char1: " + letra1 + ", Char2: " + letra2);
     }
 }
