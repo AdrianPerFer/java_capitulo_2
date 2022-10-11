e70a974c827a7666a2478c9494d592dae68af9e8
Adrian <chopon007@gmail.com>
2022-10-11 19:57:09 +0200

    Ejercicio2 changed

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,18 +1,24 @@
-/******************************************************************************************
- * 2.   Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por       *
- *      pantalla de tal forma que el resultado del programa sea el mismo que en el        *
- *      ejercicio 1 del capitulo 1.                                                       *
- *                                                                                        *
- ******************************************************************************************
- *                                                                    |  |                *
- *                                                                    |  |                *
- *                          @author Adrián Perogil Fernández        *      *              *
- *                                                                   ******               *
- ******************************************************************************************/
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         2.   Variables                                                           *
+    *                                                                                  *
+    *         2.   Crea la variable nombre y asígnale tu nombre completo.              *
+    *              Muestra su valor por pantalla de tal forma que el resultado         *
+    *              del programa sea el mismo que en el ejercicio 1 del capítulo 1.     *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // Variables "nombre" created
+        //Variable "nombre"
         String nombre = "Adrián Perogil Fernández";
+        //Output
         System.out.println(nombre);
     }
 }
